ae36b4971f99c9ff5280bc992d46b1eac8e11b38
DumDumbIch <dumdumbich@mail.ru>
2025-01-15 14:02:06 +0300

    Logger : добавил контроль потока выполнения

--- app/src/main/java/ru/dumdumbich/android/steward/tools/logger/LoggerImpl.kt
+++ app/src/main/java/ru/dumdumbich/android/steward/tools/logger/LoggerImpl.kt
@@ -2,15 +2,16 @@ package ru.dumdumbich.android.steward.tools.logger
 
 import timber.log.Timber
 
+
 class LoggerImpl(
     val target: Any,
     val isDebug: Boolean
 ) : Logger {
 
     private val key: String = "@@@"
-    private val tag = "$key ${target.javaClass.simpleName} : ${target.hashCode()}"
+    private val tag = "$key [${Thread.currentThread().name}] ${target.javaClass.simpleName} : ${target.hashCode()}"
 
     override fun toConsole(message: String) {
-        if (isDebug) Timber.tag("$key $tag").d(message)
+        if (isDebug) Timber.tag(tag).d(message)
     }
 }
